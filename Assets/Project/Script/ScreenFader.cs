e70c6d8a20c591b6eaf7fdcb967abf87b290f3ae
KewpieBinar <70208514+KewpieBinar@users.noreply.github.com>
2022-12-01 17:30:35 +0700

    ScreenFader

---
+++ Assets/Project/Script/ScreenFader.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour
+{
+    CanvasGroup _canvasGroup;
+    [SerializeField] float fadeTime;
+    Coroutine currentCor = null;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        if (currentCor != null) return;
+        currentCor = StartCoroutine(FadeIn(fadeTime));
+    }
+
+    public void FadeOut()
+    {
+        if (currentCor != null) return;
+        currentCor = StartCoroutine(FadeOut(fadeTime));
+    }
+
+    public IEnumerator FadeOut(float time)
+    {
+        _canvasGroup.alpha = 0;
+        while (_canvasGroup.alpha < 1)
+        {
+            _canvasGroup.alpha += Time.deltaTime / time;
+            yield return null;
+        }
+        currentCor = null;
+    }
+
+
+    public IEnumerator FadeIn(float time)
+    {
+        _canvasGroup.alpha = 1;
+        while (_canvasGroup.alpha > 0)
+        {
+            _canvasGroup.alpha -= Time.deltaTime / time;
+            yield return null;
+        }
+        currentCor = null;
+    }
+}
